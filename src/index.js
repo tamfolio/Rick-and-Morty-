36c05fb1a9e467db6ec5189c68f8ae494ae33aab
unknown <tamiloreolaniyi@gmail.com>
2022-04-20 22:45:50 +0100

    first committ

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
 
 const client = new ApolloClient({
@@ -14,9 +15,11 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <ApolloProvider client={client}>
     <App />
     </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
